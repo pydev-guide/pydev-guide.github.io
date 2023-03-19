f717868de57c7df0898a8a40239e79435df2ac32
Talley Lambert <talley.lambert@gmail.com>
2023-03-19 15:13:47 -0400

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://astro.build`,
+    integrations: [
+        // Enable Preact to support Preact JSX components.
+        preact(),
+        // Enable React for the Algolia search component.
+        react(),
+        tailwind(),
+    ],
+    site: `https://astro.build`,
 });
